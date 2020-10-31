a7bb70f9bfc88626fd65053022e76a58ab3091ba
Elias Campos <ecampose76@gmail.com>
2020-10-31 01:51:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,8 +11,10 @@ end
 
 introduction_with_language("Elias", "Ruby")
 
-
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+  introduction_with_language_optional("Elias", "Python")
 
 
 
